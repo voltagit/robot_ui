90be3904f549cf9690631801f9479bcb73177ec6
banaspih <banaspih@gamil.com>
2021-06-11 14:37:27 +0700

    first commit

---
+++ 2-button/main.js
@@ -0,0 +1,137 @@
+// ws://54.147.26.233:9090
+
+//var MJPEGCANVAS = require('http://cdn.robotwebtools.org/mjpegcanvasjs/current/mjpegcanvas.min.js')
+//<script type="text/javascript" src="http://cdn.robotwebtools.org/mjpegcanvasjs/current/mjpegcanvas.min.js"></script>
+
+
+
+var app = new Vue({
+    el: '#app',
+    // computed values
+    
+    
+    computed: {
+        ws_address: function() {
+            return (this.rosbridge_address)
+        },
+    },
+    // storing the state of the page
+    data: {
+        connected: false,
+        ros: null,
+        logs: [],
+        loading: false,
+        topic: null,
+        message: null,
+        rosbridge_address: '',
+        port: '9090',
+    },
+    // helper methods to connect to ROS
+    methods: {
+        connect: function() {
+            this.loading = true
+            this.ros = new ROSLIB.Ros({
+                url: this.ws_address
+            })
+            this.ros.on('connection', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Connected!')
+                this.connected = true
+                this.loading = false
+                this.setCamera()
+                this.setCamera2()
+            })
+            this.ros.on('error', (error) => {
+                this.logs.unshift((new Date()).toTimeString() +  - Error)
+            })
+            this.ros.on('close', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Disconnected!')
+                this.connected = false
+                this.loading = false
+                document.getElementById('divCamera').innerHTML = ''
+                document.getElementById('divCamera2').innerHTML = ''
+            })
+        },
+        disconnect: function() {
+            this.ros.close()
+        },
+        setTopic: function() {
+            this.topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+        },
+        forward: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 1, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        stop: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 0, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        backward: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: -1, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        turnLeft: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 0.5, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0.5, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        turnRight: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 0.5, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: -0.5, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+         setCamera: function() {
+            let without_wss = this.rosbridge_address.split('ws://')[1]
+            console.log(without_wss)
+            let domain = without_wss.split('/')[0] + '/' + without_wss.split('/')[1]
+            console.log(domain)
+            let host = domain + '/cameras'
+            let viewer = new MJPEGCANVAS.Viewer({
+                divID: 'divCamera1',
+                host: host,
+                width: 320,
+                height: 240,
+                topic: '/stream?topic=/image_raw',
+                ssl: true,
+            })
+        },
+        setCamera2: function() {
+            let without_wss = this.rosbridge_address.split('ws://')[1]
+            console.log(without_wss)
+            let domain = without_wss.split('/')[0] + '/' + without_wss.split('/')[1]
+            console.log(domain)
+            let host = domain + '/cameras'
+            let viewer = new MJPEGCANVAS.Viewer({
+                divID: 'divCamera2',
+                host: host,
+                width: 320,
+                height: 240,
+                topic: '/camera/depth/image_raw',
+                ssl: true,
+            })
+        },
+    },
+    mounted() {
+    },
+})
